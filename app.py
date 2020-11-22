2dd874a0b4c063fbdb80f704446ac69856989844
Nir Lotan <nir.lotan@intel.com>
2020-11-22 20:39:08 +0200

    update model version, small improvements to the analogy finder

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from gensim.test.utils import common_texts, get_tmpfile
 from gensim.models import Word2Vec
 
 vector_size = 100
-model_name = "item2vec_v2_350.model"
+model_name = "item2vec_v3_350.model"
 
 def id_to_name(uid):
     return ud_df[ud_df['user_id']==uid]['screen_name'].to_string(index=False).strip()
@@ -32,8 +32,18 @@ def tsnescatterplot(model, word, list_names):
         word_labels.append(wrd_score[0])
         arrays = np.append(arrays, wrd_vector, axis=0)
 
-    word_labels = [ud_df[ud_df['user_id']==wrd]['screen_name'].to_string(index=False).strip() for wrd in word_labels]
-    return word_labels
+#    word_labels = [ud_df[ud_df['user_id']==wrd]['screen_name'].to_string(index=False).strip() for wrd in word_labels]
+
+    word_output = []
+    for wrd in word_labels:
+        user_name = ud_df[ud_df['user_id']==wrd]['screen_name'].to_string(index=False).strip()
+        if ( user_name != 'Series([], )'):
+            word_output.append(user_name)
+        else:
+            with open("missing.log", "a") as myfile:
+                myfile.write(wrd+'\n')
+
+    return word_output
     
     
 def ten_most_similar(wrd):
@@ -48,9 +58,24 @@ def analogy(namea, nameb, namec):
     idb = name_to_id(nameb)
     idc = name_to_id(namec)
 
+    if (ida == 'Series([], )'):
+        st.write( f'User named {namea} is not in my database')
+    if (idb == 'Series([], )'):
+        st.write( f'User named {nameb} is not in my database')
+    if (idc == 'Series([], )'):
+        st.write( f'User named {namec} is not in my database')
+        
     result = w2v_model.most_similar(negative=[ida], 
                                 positive=[idb, idc])
-    return id_to_name(result[0][0])
+
+
+    res_name = id_to_name(result[0][0])
+    if (res_name == 'Series([], )'):
+        with open("missing.log", "a") as myfile:
+            myfile.write(result[0][0]+'\n')
+        return result[0][0]
+    else:
+        return id_to_name(result[0][0])
 
 def make_clickable(link):
     # target _blank to open new window
@@ -120,6 +145,7 @@ elif (selected_task == 'Find similar users'):
 	except:
 		st.write('I can\'t find ' +user_input+ ' in my dataset. Please try different spelling or different user (sometimes upper/lowercase helps)')
 
+
 	# link is the column with hyperlinks
 	result_df['URL'] = result_df['URL'].apply(make_clickable)
 	result_df['Wiki'] = result_df['Wiki'].apply(make_clickable)
@@ -131,10 +157,16 @@ if (selected_task == 'Analogy game'):
 	st.write('Write three Twitter user names (exact names).')
 	st.write('we take the analogy of the first two users and apply on the third')
 	
-	user1 = st.text_input("Twitter user1: ", '')
-	user2 = st.text_input("is to user2:", '')
-	user3 = st.text_input("like user3:", '')
-	st.write('is to: ')
-	if st.button('Go'):
-		res = analogy(user1,user2,user3)
-		st.write(user1 + ' is to ' + user2 + ' like ' + user3 + ' is to ' + res)
+	c1, c2, c3, c4 = st.beta_columns(4)
+
+	user1 = c1.text_input("Twitter user: ", '')
+	user2 = c2.text_input("is to:", '')
+	user3 = c3.text_input("like:", '')
+	try:
+		res = ''
+		if st.button('Go'):
+			res = analogy(user1,user2,user3)
+			st.write(user1 + ' is to ' + user2 + ' like ' + user3 + ' is to ' + res)
+		c4.text_input('is to:',res)
+	except:
+		st.write('')
